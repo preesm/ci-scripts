598f24d5f0ccafe0e6219103c71f0a6dc399462d
Clément Guy <clement.guy@insa-rennes.fr>
2015-04-30 16:01:01 +0200

    Updated preesm_run_workflow.sh.

--- headless_execution/preesm/preesm_run_workflow.sh
+++ headless_execution/preesm/preesm_run_workflow.sh
@@ -1,14 +1,10 @@
-#!/bin/bash
-
-source $(dirname $0)/preesm_defines.sh
-
 NBARGS=3
 function print_usage() {
     echo
-    echo "Usage: $0 <working_directory> <project_name> <workspace_directory> [<plugin_dir> <plugin_dir> ...]"
-    echo "    <working_directory>           Path to folder used to perform build & tests"
-    echo "    <project_path>              	Path to the project containing the workflow and scenario to execute"
-    echo "	  <workspace_directory>			Path to the workspace where to execute"
+    echo "Usage: $0 <workspace> <eclipse_directory> <project> [<workflow> <scenario>]"
+    echo "    <workspace>                   Path to eclipse workspace to use"
+    echo "    <eclipse_directory>           Path to folder containing eclipse installation to use"
+    echo "    <project>                     Name of the project containing the workflow and the scenario to execute"
 }
 
 if [ $# -lt $NBARGS ]; then
@@ -16,23 +12,25 @@ if [ $# -lt $NBARGS ]; then
     exit $E_BADARGS
 fi
 
-[ ! -d "$2" ] && echo "Missing features directory" && print_usage && exit $E_BADARGS
+[ ! -d "$1" ] && echo "Missing application directory" && print_usage && exit $E_BADARGS
+[ ! -d "$2" ] && echo "Missing workspace directory" && print_usage && exit $E_BADARGS
 
-PROJECT=$2
-RUNWORKSPACE=$3
+WORKSPACE=$1
+ECLIPSEDIR=$2
+PROJECT=$3
+WORKFLOW=$4
+SCENARIO=$5
 
-echo "***START*** $0 $(date -R)"
+if [ -z $WORKFLOW ]; then
+    WORKFLOWMSG="workflow(s)"
+else
+    WORKFLOWMSG="workflow: $WORKFLOW"
+fi
+if [ -z $SCENARIO ]; then
+    SCENARIOMSG="scenario(s)"
+else
+    SCENARIOMSG="scenario: $SCENARIO"
+fi
 
-echo "Register projects in eclipse workspace"
-$ECLIPSERUN/eclipse     -nosplash -consoleLog \
-                        -application net.sf.orcc.cal.workspaceSetup \
-                        -data $RUNWORKSPACE \
-                        $APPDIR
-
-echo "Run workflow from project $PROJECT"
-$ECLIPSERUN/eclipse     -nosplash -consoleLog \
-                        -application org.ietr.preesm.cli \
-                        -data $RUNWORKSPACE \
-                        $PROJECT
-
-echo "***END*** $0 $(date -R)"
+echo "   Run $WORKFLOWMSG and $SCENARIOMSG"
+$ECLIPSEDIR/eclipse -nosplash -consoleLog -application org.ietr.preesm.cli.workflowCli -data $WORKSPACE $PROJECT $WORKFLOW $SCENARIO
